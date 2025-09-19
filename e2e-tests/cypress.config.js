6452e8995654803e1d0ee913bacf64dfe461c966
Lucas <lucasrezendelrs@gmail.com>
2025-09-19 11:49:31 -0300

    Adiciona task removerUser para remover usuário para que a massa de teste seja sempre válida

--- e2e-tests/cypress.config.js
+++ e2e-tests/cypress.config.js
@@ -1,12 +1,27 @@
 const { defineConfig } = require('cypress')
+const Database = require('better-sqlite3')
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     viewportWidth: 1366,
     viewportHeight: 900,
-    fixturesFolder: 'cypress/e2e/fixtures'
-  },
-  fixturesFolder: false,
-  video: false,
+    fixturesFolder: false,
+    video: false,
+    fixturesFolder: 'cypress/e2e/fixtures',
+    setupNodeEvents(on, config) {
+      const db = new Database('C:\\Users\\Lucas\\Documents\\Estudos\\QA\\hub-de-leitura\\database\\biblioteca.db', { verbose: console.log })
+      on('task', {
+        removeUser(email) {
+          try {
+            const stmt = db.prepare('DELETE FROM Users WHERE email = ?')
+            const result = stmt.run(email)
+            return { success: result.changes > 0 }
+          } catch (error) {
+            throw error
+          }
+        }
+      })
+    }
+  }
 })
--- e2e-tests/cypress/e2e/gui/signup.cy.js
+++ e2e-tests/cypress/e2e/gui/signup.cy.js
@@ -8,100 +8,73 @@ describe('Criar conta', function () {
         })
 
     })
-    context('Cadastro válido com todos os campos preenchidos corretamente ', function () {
-
+    context.only('Cadastro válido com todos os campos preenchidos corretamente ', function () {
+        before(function () {
+            cy.task('removeUser', this.valid.email).then(function (result) {
+                console.log(result)
+            })
+        })
         it('Deve cadastrar com sucesso', function () {
             signup.go()
             signup.form(this.valid)
             signup.submit()
 
             cy.url().should('include', '/dashboard.html')
-            cy.get('span[id=user-name]')
-                .should('be.visible')
-                .and('have.text', this.valid.name)
+                cy.get('span[id=user-name]')
+                    .should('be.visible')
+                    .and('have.text', this.valid.name)
 
 
-        })
-
-    })
-    context('Cadastro sem preencher Nome Completo  ', function () {
-        const alert = 'Nome deve ter pelo menos 2 caracteres'
-        const name = " "
-
-
-        it('Deve exibir ' + alert, function () {
-            this.valid.name = name
-
-            signup.go()
-            signup.form(this.valid)
-            signup.submit()
-
-            cy.get('#name + .invalid-feedback')
-                .should('be.visible')
-                .and('have.text', alert)
+            })
 
         })
-
-    })
-    context('Cadastro com Nome Completo menor que 2 caracteres', function () {
-        const alert = 'Nome deve ter pelo menos 2 caracteres'
-        const name = 'A'
+        context('Cadastro sem preencher Nome Completo  ', function () {
+            const alert = 'Nome deve ter pelo menos 2 caracteres'
+            const name = " "
 
 
-        it('Deve exibir ' + alert, function () {
-            this.valid.name = name
+            it('Deve exibir ' + alert, function () {
+                this.valid.name = name
 
-            signup.go()
-            signup.form(this.valid)
-            signup.submit()
+                signup.go()
+                signup.form(this.valid)
+                signup.submit()
 
-            cy.get('#name + .invalid-feedback')
-                .should('be.visible')
-                .and('have.text', alert)
+                cy.get('#name + .invalid-feedback')
+                    .should('be.visible')
+                    .and('have.text', alert)
 
-        })
-
-    })
-    context('Cadastro sem preencher Email', function () {
-        const alert = 'Email válido é obrigatório'
-        const email = " "
-
-
-        it('Deve exibir ' + alert, function () {
-            this.valid.email = email
-
-            signup.go()
-            signup.form(this.valid)
-            signup.submit()
-
-            cy.get('#email + .invalid-feedback')
-                .should('be.visible')
-                .and('have.text', alert)
+            })
 
         })
+        context('Cadastro com Nome Completo menor que 2 caracteres', function () {
+            const alert = 'Nome deve ter pelo menos 2 caracteres'
+            const name = 'A'
 
-    })
 
-    context('Cadastro com formato inválido de E-mail', function () {
-        const emails = [
-            'lucas.com.br',
-            'yahoo.com',
-            '@gmail.com',
-            '@',
-            'lucas@',
-            '111',
-            '&*^&&^*',
-            'xpto123'
-        ]
-        beforeEach(function () {
-            signup.go()
+            it('Deve exibir ' + alert, function () {
+                this.valid.name = name
+
+                signup.go()
+                signup.form(this.valid)
+                signup.submit()
+
+                cy.get('#name + .invalid-feedback')
+                    .should('be.visible')
+                    .and('have.text', alert)
+
+            })
+
         })
-        emails.forEach(function (email) {
+        context('Cadastro sem preencher Email', function () {
             const alert = 'Email válido é obrigatório'
+            const email = " "
 
-            it('Não deve criar conta com o email invalido: ' + email, function () {
+
+            it('Deve exibir ' + alert, function () {
                 this.valid.email = email
 
+                signup.go()
                 signup.form(this.valid)
                 signup.submit()
 
@@ -112,89 +85,120 @@ describe('Criar conta', function () {
             })
 
         })
-    })
-    context('Cadastro sem preencher Senha', function () {
-        const alert = 'Por favor, crie uma senha com pelo menos 6 caracteres.'
-        const password = " "
+
+        context('Cadastro com formato inválido de E-mail', function () {
+            const emails = [
+                'lucas.com.br',
+                'yahoo.com',
+                '@gmail.com',
+                '@',
+                'lucas@',
+                '111',
+                '&*^&&^*',
+                'xpto123'
+            ]
+            beforeEach(function () {
+                signup.go()
+            })
+            emails.forEach(function (email) {
+                const alert = 'Email válido é obrigatório'
+
+                it('Não deve criar conta com o email invalido: ' + email, function () {
+                    this.valid.email = email
+
+                    signup.form(this.valid)
+                    signup.submit()
+
+                    cy.get('#email + .invalid-feedback')
+                        .should('be.visible')
+                        .and('have.text', alert)
+
+                })
+
+            })
+        })
+        context('Cadastro sem preencher Senha', function () {
+            const alert = 'Por favor, crie uma senha com pelo menos 6 caracteres.'
+            const password = " "
 
 
-        it('Deve exibir ' + alert, function () {
-            this.valid.password = password
+            it('Deve exibir ' + alert, function () {
+                this.valid.password = password
 
-            signup.go()
-            signup.form(this.valid)
+                signup.go()
+                signup.form(this.valid)
 
-            cy.get('[id=password]')
-                .clear() // garante que o campo fique vazio
-                .should('have.value', '')
+                cy.get('[id=password]')
+                    .clear() // garante que o campo fique vazio
+                    .should('have.value', '')
 
-            signup.submit()
+                signup.submit()
 
-            cy.get('#password-strength + .invalid-feedback')
-                .should('be.visible')
-                .and('have.text', alert)
+                cy.get('#password-strength + .invalid-feedback')
+                    .should('be.visible')
+                    .and('have.text', alert)
+
+            })
+
+        })
+        context('Cadastro com senha fraca', function () {
+            const alert = 'Por favor, crie uma senha com pelo menos 6 caracteres.'
+            const password = "123"
+
+
+            it('Deve exibir ' + alert, function () {
+                this.valid.password = password
+                this.valid.confirmPassword = password
+
+                signup.go()
+                signup.form(this.valid)
+
+
+                signup.submit()
+
+                cy.get('#password-strength + .invalid-feedback')
+                    .should('be.visible')
+                    .and('have.text', alert)
+
+            })
 
         })
 
-    })
-    context('Cadastro com senha fraca', function () {
-        const alert = 'Por favor, crie uma senha com pelo menos 6 caracteres.'
-        const password = "123"
+        context('Cadastro com Senha e Confirmação diferentes', function () {
+            const alert = 'Senhas não coincidem'
 
+            it('Deve exibir ' + alert, function () {
+                signup.go()
+                signup.form(this.invalidPassword)
 
-        it('Deve exibir ' + alert, function () {
-            this.valid.password = password
-            this.valid.confirmPassword = password
+                signup.submit()
 
-            signup.go()
-            signup.form(this.valid)
+                cy.get('#confirm-password + .invalid-feedback')
+                    .should('be.visible')
+                    .and('have.text', alert)
 
-
-            signup.submit()
-
-            cy.get('#password-strength + .invalid-feedback')
-                .should('be.visible')
-                .and('have.text', alert)
+            })
 
         })
 
-    })
+        context('Cadastro sem marcar checkbox de aceite dos termos ', function () {
+            const alert = 'Você deve aceitar os termos de uso'
 
-    context('Cadastro com Senha e Confirmação diferentes', function () {
-        const alert = 'Senhas não coincidem'
 
-        it('Deve exibir ' + alert, function () {
-            signup.go()
-            signup.form(this.invalidPassword)
+            it('Deve exibir ' + alert, function () {
+                signup.go()
+                signup.form(this.invalidTerms)
 
-            signup.submit()
+                signup.submit()
 
-            cy.get('#confirm-password + .invalid-feedback')
-                .should('be.visible')
-                .and('have.text', alert)
+                cy.get('#terms-agreement + .invalid-feedback')
+                    .should('be.visible')
+                    .and('have.text', alert)
 
-        })
-
-    })
-
-    context('Cadastro sem marcar checkbox de aceite dos termos ', function () {
-        const alert = 'Você deve aceitar os termos de uso'
-    
-
-        it('Deve exibir ' + alert, function () {
-            signup.go()
-            signup.form(this.invalidTerms)
-
-            signup.submit()
-
-            cy.get('#terms-agreement + .invalid-feedback')
-                .should('be.visible')
-                .and('have.text', alert)
+            })
 
         })
 
+
+
     })
-
-
-
-})
